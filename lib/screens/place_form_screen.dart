40a3bb74dc4953400ccfca448e51b34e5143cbce
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-04 13:57:46 -0300

    adicionando formulario novo lugar

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,21 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({Key? key}) : super(key: key);
 
   @override
-  _PlaceFormScreenState createState() => _PlaceFormScreenState();
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Novo Lugar'),
-        ),
-        body: const Center(
-          child: Text('Aqui tem um formulário'),
-        ));
+      appBar: AppBar(
+        title: const Text('Novo Lugar'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            onPressed: _submitForm,
+          ),
+        ],
+      ),
+    );
   }
 }
