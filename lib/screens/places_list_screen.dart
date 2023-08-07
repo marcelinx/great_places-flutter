df812036ac17b264e7b6fd73f2acb6942d5db9bf
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-07 11:32:41 -0300

    adicionando componente lista de lugares

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -18,10 +20,22 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(
-          color: Colors.amber,
+      body: Consumer<GreatPlaces>(
+        child: const Center(
+          child: Text('Nenhum local cadastrado'),
         ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+            ? ch!
+            : ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          FileImage(greatPlaces.itemByIndex(i).image),
+                    ),
+                    title: Text(greatPlaces.itemByIndex(i).title),
+                    onTap: () {}),
+              ),
       ),
     );
   }
